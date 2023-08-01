d969db0d32cb1144e71e302dc4b6dae05173bddc
alidiab2000 <53220716+alidiab2000@users.noreply.github.com>
2023-08-01 16:45:29 +0300

    Task  done

--- app/src/main/java/com/alidiab/myapplication/MainActivity.kt
+++ app/src/main/java/com/alidiab/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.alidiab.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()  {
+    lateinit var edit :EditText
+    lateinit var btn :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        edit = findViewById(R.id.edit_Text_iti)
+        btn = findViewById(R.id.showInput_iti)
+        btn.setOnClickListener{
+            println(edit.text.toString())
+         }
+      }
+
+
+
+
 }
