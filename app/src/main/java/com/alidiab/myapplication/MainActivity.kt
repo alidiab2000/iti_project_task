06d2a2b3477a21c0bc3c8ad2a5c45ae8b5376d7a
alidiab2000 <53220716+alidiab2000@users.noreply.github.com>
2023-08-01 17:09:09 +0300

    Task  done_

--- app/src/main/java/com/alidiab/myapplication/MainActivity.kt
+++ app/src/main/java/com/alidiab/myapplication/MainActivity.kt
@@ -2,22 +2,19 @@ package com.alidiab.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import com.alidiab.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity()  {
-    lateinit var edit :EditText
-    lateinit var btn :Button
+    lateinit var biding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        edit = findViewById(R.id.edit_Text_iti)
-        btn = findViewById(R.id.showInput_iti)
-        btn.setOnClickListener{
-            println(edit.text.toString())
-         }
+        biding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biding.root)
+       biding.showInputIti.setOnClickListener{
+           print(biding.showInputIti.text.toString())
+       }
+
       }
 
 
