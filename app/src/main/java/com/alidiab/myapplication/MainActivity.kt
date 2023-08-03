3985bca58b77ac1a87d393e401915068f0a7a694
alidiab2000 <53220716+alidiab2000@users.noreply.github.com>
2023-08-03 16:34:01 +0300

    login form task

--- app/src/main/java/com/alidiab/myapplication/MainActivity.kt
+++ app/src/main/java/com/alidiab/myapplication/MainActivity.kt
@@ -3,7 +3,12 @@ package com.alidiab.myapplication
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.get
 import com.alidiab.myapplication.databinding.ActivityMainBinding
 
 
@@ -14,10 +19,32 @@ class MainActivity : AppCompatActivity()  {
         biding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(biding.root)
 
-        val loginButton: Button = findViewById(R.id.loginButton)
-        loginButton.setBackgroundColor(Color.RED)
+       biding.loginButton.setOnClickListener{
+           val userText : String  = biding.editTextUsername.text.toString()
+           val userempty = biding.editTextUsername.text.isEmpty()
+           val passempty = biding.editTextUsername.text.isEmpty()
+           val passText : String  = biding.editPass.text.toString()
+           val gender   = if(biding.radiomale.isChecked) biding.radiomale.text.toString()
+           else if(biding.radiofemale.isChecked)
+               biding.radiofemale.text.toString()
+           else {
+               "plz chose gender"
+           }
+           if(passempty && userempty || gender =="plz chose gender" ){
+               Toast.makeText(this , "Not vald email or pass or gender" , Toast.LENGTH_LONG).show()
+           }
 
-      }
+           else {
+               Toast.makeText(
+                   this,
+                   "Your Email is $userText and your pass is $passText  and your gender is $gender",
+                   Toast.LENGTH_LONG
+               ).show()
+           }
+       }
+
+
+    }
 
 
 
